0527ddce2af7331aa3e6498dd73d9bdc9e2c3422
noiqum <onurcoskun99@gmail.com>
2024-03-09 17:07:13 +0300

    toogle button style

--- src/Components/ContentToogle/ContentToogle.tsx
+++ src/Components/ContentToogle/ContentToogle.tsx
@@ -1,5 +1,8 @@
 import { updateContent } from "../../store/contentSlice";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { ListIcon } from "./ListIcon";
+import { MapIcon } from "./MapIcon";
+import "./ContentToogle.scss";
 
 export const ContentToogle = () => {
   const { displayContent } = useAppSelector((state) => state.content);
@@ -7,27 +10,35 @@ export const ContentToogle = () => {
   return (
     <div className="content__toogle">
       <label
-        onChange={() => dispatch(updateContent("map"))}
-        className="content__toogle__label--map"
-        htmlFor="content"
+        className={`content__toogle__label--map ${
+          displayContent === "map" ? "active" : ""
+        }`}
+        htmlFor="map"
       >
+        <MapIcon></MapIcon>
+        <span>Map</span>
         <input
           checked={displayContent === "map"}
           type="radio"
-          name="map"
+          name="content"
           id="map"
+          onChange={() => dispatch(updateContent("map"))}
         />
       </label>
       <label
-        onChange={() => dispatch(updateContent("list"))}
-        className="content__toogle__label--list"
-        htmlFor="content"
+        className={`content__toogle__label--list ${
+          displayContent === "list" ? "active" : ""
+        }`}
+        htmlFor="list"
       >
+        <ListIcon></ListIcon>
+        <span>List</span>
         <input
           checked={displayContent === "list"}
           type="radio"
-          name="list"
+          name="content"
           id="list"
+          onChange={() => dispatch(updateContent("list"))}
         />
       </label>
     </div>
