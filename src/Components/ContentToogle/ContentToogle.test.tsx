e08f73334a3bdbc8a30c5fe5f5cb1950c09eefce
noiqum <onurcoskun99@gmail.com>
2024-03-09 17:53:25 +0300

    content toogle test

--- src/Components/ContentToogle/ContentToogle.test.tsx
+++ src/Components/ContentToogle/ContentToogle.test.tsx
@@ -1,3 +1,4 @@
+import { screen } from "@testing-library/react";
 import { renderWithProviders } from "../../test-utils";
 import { ContentToogle } from "./ContentToogle";
 
@@ -5,5 +6,28 @@ describe("ContentToogle", () => {
   it("should render", () => {
     const { container } = renderWithProviders(<ContentToogle />);
     expect(container).toBeInTheDocument();
+  });
+  test("initial state should be map", () => {
+    renderWithProviders(<ContentToogle />);
+    const mapLabel = screen.getByLabelText("Map");
+    const listLabel = screen.getByLabelText("List");
+    expect(mapLabel).toBeChecked();
+    expect(listLabel).not.toBeChecked();
+  });
+  test("should switch to list", () => {
+    renderWithProviders(<ContentToogle />);
+    const listLabel = screen.getByLabelText("List");
+    listLabel.click();
+    expect(listLabel).toBeChecked();
+    expect(screen.getByLabelText("Map")).not.toBeChecked();
+  });
+  test("should switch to map", () => {
+    renderWithProviders(<ContentToogle />);
+    const mapLabel = screen.getByLabelText("Map");
+    const listLabel = screen.getByLabelText("List");
+    listLabel.click();
+    mapLabel.click();
+    expect(mapLabel).toBeChecked();
+    expect(screen.getByLabelText("List")).not.toBeChecked();
   });
 });
