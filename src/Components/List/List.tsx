4fe60f2094ababe4957f3fede51781285cea39dd
noiqum <onurcoskun99@gmail.com>
2024-03-10 16:41:07 +0300

    feat: searchbar list filter functionality

--- src/Components/List/List.tsx
+++ src/Components/List/List.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { TRegion } from "../../API/serviceTypes";
 import "./List.scss";
 
@@ -7,10 +8,23 @@ interface ListProps {
   searchParam: string;
 }
 
-export const List = ({ regions, open }: ListProps) => {
+export const List = ({ regions, open, searchParam }: ListProps) => {
+  const [filteredRegions, setFilteredRegions] = useState(regions);
+
+  useEffect(() => {
+    if (searchParam.length > 2) {
+      const filtered = regions.filter((region) => {
+        return region.name.toLowerCase().includes(searchParam.toLowerCase());
+      });
+      setFilteredRegions(filtered);
+    }
+    if (searchParam.length === 0) {
+      setFilteredRegions(regions);
+    }
+  }, [searchParam, regions]);
   return (
     <ul id="searchlist" className={`List ${open ? "List--open" : ""}`}>
-      {regions.map((region) => {
+      {filteredRegions.map((region) => {
         return (
           <li key={region.name} className="List__item">
             {region.name}
