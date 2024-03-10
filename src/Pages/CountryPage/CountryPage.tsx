0969166aa19b3df323e612ea469b027d11e8bd02
noiqum <onurcoskun99@gmail.com>
2024-03-11 01:03:01 +0300

    feat: not found page redirection

--- src/Pages/CountryPage/CountryPage.tsx
+++ src/Pages/CountryPage/CountryPage.tsx
@@ -1,18 +1,29 @@
 import { useEffect } from "react";
 import { useAppDispatch, useAppSelector } from "../../store/hooks";
-import { useLocation, redirect, useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 export const CountryPage = () => {
   const { country } = useParams();
+  const navigate = useNavigate();
   const { list } = useAppSelector((state) => state.list);
   const dispatch = useAppDispatch();
   useEffect(() => {
     if (list.length === 0) {
       dispatch({ type: "SET_LIST" });
     } else {
-      console.log("params", country);
+      const countryName = country?.toLowerCase().trim();
+      console.log("country", countryName);
+      const contIndex = list.findIndex(
+        (country) => country.name.toLowerCase().trim() === countryName
+      );
+      if (contIndex === -1) {
+        navigate("/not-found");
+      }
     }
-  }, [list, dispatch, country]);
+  }, [list, dispatch, country, navigate]);
+  if (!country) {
+    return <div>CountryPage</div>;
+  }
 
   return <div className="CountryPage">CountryPage</div>;
 };
