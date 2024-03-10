bc2e01c2694d37c0200cd0a588eff040884ba4e9
noiqum <onurcoskun99@gmail.com>
2024-03-10 22:59:19 +0300

    chore: routes update

--- src/Components/Routes/Routes.tsx
+++ src/Components/Routes/Routes.tsx
@@ -1,9 +1,18 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../../App";
+import { CountryPage } from "../../Pages/CountryPage/CountryPage";
 
 export const Router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "country/:country",
+    element: <CountryPage />,
+  },
+  {
+    path: "/not-found",
+    element: <div>404</div>,
   },
 ]);
---
+++ src/Pages/CountryPage/CountryPage.tsx
@@ -0,0 +1,3 @@
+export const CountryPage = () => {
+  return <div className="CountryPage">CountryPage</div>;
+};
