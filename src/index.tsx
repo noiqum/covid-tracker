34de03dc55b0253006578704322ab0d73aa0de95
noiqum <onurcoskun99@gmail.com>
2024-03-10 22:38:29 +0300

    config: router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import { RouterProvider } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import { Router } from "./Components/Routes/Routes";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +13,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={Router} />
     </Provider>
   </React.StrictMode>
 );
