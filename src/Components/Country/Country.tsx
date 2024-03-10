5e562a06ba35e309f57255bd1a9b951c33d45197
noiqum <onurcoskun99@gmail.com>
2024-03-11 00:37:38 +0300

    feat: country click redirect to country page on map

--- src/Components/Country/Country.tsx
+++ src/Components/Country/Country.tsx
@@ -1,6 +1,11 @@
 import { TCountry } from "../../Data/Data";
 import "./Country.scss";
+import { Link } from "react-router-dom";
 
 export const Country = ({ name, path }: TCountry) => {
-  return <path className="country" d={path} name={name}></path>;
+  return (
+    <Link to={`/country/${name.toLowerCase().trim()}`}>
+      <path className="country" d={path} name={name}></path>
+    </Link>
+  );
 };
--- src/Pages/CountryPage/CountryPage.tsx
+++ src/Pages/CountryPage/CountryPage.tsx
@@ -1,3 +1,18 @@
+import { useEffect } from "react";
+import { useAppDispatch, useAppSelector } from "../../store/hooks";
+import { useLocation, redirect, useParams } from "react-router-dom";
+
 export const CountryPage = () => {
+  const { country } = useParams();
+  const { list } = useAppSelector((state) => state.list);
+  const dispatch = useAppDispatch();
+  useEffect(() => {
+    if (list.length === 0) {
+      dispatch({ type: "SET_LIST" });
+    } else {
+      console.log("params", country);
+    }
+  }, [list, dispatch, country]);
+
   return <div className="CountryPage">CountryPage</div>;
 };
