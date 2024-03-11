eff8bee0d2154e5b70d702d681e8a6aea5d3edd1
noiqum <onurcoskun99@gmail.com>
2024-03-11 13:23:14 +0300

    fix: united states name mismatch

--- src/Components/Country/Country.tsx
+++ src/Components/Country/Country.tsx
@@ -1,10 +1,17 @@
+import { useCallback } from "react";
 import { TCountry } from "../../Data/Data";
 import "./Country.scss";
 import { Link } from "react-router-dom";
 
 export const Country = ({ name, path }: TCountry) => {
+  const linkUrlHandler = useCallback((name: string) => {
+    if (name === "United States") {
+      return `/country/us`;
+    }
+    return `/country/${name.toLowerCase().replace(/\s/g, "").trim()}`;
+  }, []);
   return (
-    <Link to={`/country/${name.toLowerCase().replace(/\s/g, "").trim()}`}>
+    <Link to={linkUrlHandler(`${name}`)}>
       <path className="country" d={path} name={name}></path>
     </Link>
   );
