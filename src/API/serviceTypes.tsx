26a84aa24089651ed2eb489525a53e28b19c3e74
noiqum <onurcoskun99@gmail.com>
2024-03-11 16:11:07 +0300

    chore: country detail service response type

--- src/API/serviceTypes.tsx
+++ src/API/serviceTypes.tsx
@@ -20,3 +20,42 @@ export type TRegion = {
   iso: string;
   name: string;
 };
+export type CountryDetail = {
+  date: string;
+  confirmed: number;
+  deaths: number;
+  recovered: number;
+  confirmed_diff: number;
+  deaths_diff: number;
+  recovered_diff: number;
+  last_update: string;
+  active: number;
+  active_diff: number;
+  fatality_rate: number;
+  region: regionResponse[];
+};
+export type regionResponse = {
+  iso: string;
+  name: string;
+  province: string;
+  lat: string;
+  long: string;
+  cities: CityDetail[];
+};
+
+export type CityDetail = {
+  name: string;
+  date: string;
+  fips: number;
+  lat: string;
+  long: string;
+  confirmed: number;
+  deaths: number;
+  confirmed_diff: number;
+  deaths_diff: number;
+  last_update: string;
+};
+
+export type CountryResponse = {
+  data: CountryDetail[];
+};
